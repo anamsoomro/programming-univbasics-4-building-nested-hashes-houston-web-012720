9f22a79586569ef0972ea195b184c90b0bbf8b90
anam soomro <anamsoomroed@gmail.com>
2019-12-26 00:04:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,8 +20,16 @@ def monopoly_with_third_tier
   # complexity.
   {:railroads => { 
     :pieces => 4, 
-    :rent_in_dollars => {:one_piece_owned => 25, :two => {}, :three => {}, :four => {}},
-    :names => {:railroad1 => {}, :railroad2 => {}, :railroad3 => {}, :railroad4 => {}}
+    :rent_in_dollars => {
+      :one_piece_owned => 25, 
+      :two_pieces_owned => 50, 
+      :three => {}, 
+      :four => {}},
+    :names => {
+      :railroad1 => {}, 
+      :railroad2 => {}, 
+      :railroad3 => {}, 
+      :railroad4 => {}}
   }
   }
 end
