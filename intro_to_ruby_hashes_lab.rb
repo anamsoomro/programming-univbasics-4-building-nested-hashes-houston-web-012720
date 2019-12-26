12dac6dce73a8368642465e2254cf4abf0ababb0
anam soomro <anamsoomroed@gmail.com>
2019-12-26 00:11:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,10 +47,10 @@ def monopoly_with_fourth_tier
       :three_pieces_owned => 100, 
       :four_pieces_owned => 200},
     :names => {
-      :reading_railroad => {}, 
-      :pennsylvania_railroad => {}, 
-      :b_and_o_railroad => {}, 
-      :shortline_railroad => {}}
+      :reading_railroad => "$100", 
+      :pennsylvania_railroad => "$200", 
+      :b_and_o_railroad => "$400", 
+      :shortline_railroad => "$800"}
    }
   }
 end
